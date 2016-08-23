f6a87cc17e9e6707027a5873a82952b1bbc16168
Mohanad Mohammad Alhasasneh <Mhasasneh@aspire-services.net>
2016-08-23 16:28:21 +0300

    Upload All Project Files

---
+++ src/Feature/Chat/Sitecore.Feature.Chat/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Sitecore.Feature.Chat.Controller
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class ChatController : Controller
+    {
+    
+        public ActionResult LiveChat()
+        {
+            Sitecore.Analytics.Data.TrackingField t = new Sitecore.Analytics.Data.TrackingField(Sitecore.Context.Item.Fields["__tracking"]);
+            List<Analytics.Data.ContentProfile> profilesList = t.Profiles.ToList();
+
+            foreach (var profile in profilesList)
+            {
+                foreach (var key in profile.Keys)
+                {
+
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult GetChatLog()
+        {
+            string csrName = Sitecore.Context.User.Name;
+
+            MongoOperations objMongoOperations = new MongoOperations();
+            List<ChatEntity> liChat = objMongoOperations.GetChatLog();
+
+            liChat = liChat.Where(p => p.From == csrName).ToList();
+
+
+            return Json(liChat, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ src/Feature/LiveChat/Sitecore.Feature.LiveChat/Commands/OpenLiveChat.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.LiveChat.Commands
+{
+    public class OpenLiveChat : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            SheerResponse.ShowModalDialog("/sitecore/shell/sitecore/client/Your Apps/LiveChat/LiveChat", "500px", "500px");
+        }
+    }
+}
