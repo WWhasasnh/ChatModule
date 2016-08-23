7de716626ad165480bf7c4d35a3a29feb1a38077
Mohanad Mohammad Alhasasneh <Mhasasneh@aspire-services.net>
2016-08-23 15:56:14 +0300

    Add files via upload

---
+++ gulp-config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var config = {
+        websiteRoot: "C:\\inetpub\\wwwroot\\Habitat.local\\Website",
+        sitecoreLibraries: "C:\\inetpub\\wwwroot\\Habitat.local\\Website\\bin",
+        solutionName: "Habitat",
+        buildConfiguration: "Debug"
+    }
+    return config;
+}
